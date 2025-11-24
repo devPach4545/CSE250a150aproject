c5638e4aa877d7e1fa798d35fd42fd940aa8d430
Anthony Wang <anthonyw40916@gmail.com>
2025-11-23 19:45:50 -0800

    Added code to analyze and plot hidden states over prices

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from hmmlearn import hmm
 
 def fetch_stock_data(ticker, start_date, end_date):
+    print(f"Downloading data for {ticker} from {start_date} to {end_date}...")
     data = yfinance.download(ticker, start=start_date, end=end_date)
     return data['Close']
 
@@ -16,13 +17,39 @@ def fit_hmm_model(prices, n_states=3):
     model = hmm.GaussianHMM(n_components=n_states, covariance_type="diag", n_iter=10000)
     
     # Run EM algorithm to fit data
+    print("Fitting HMM to data...")
     model.fit(X)
     
     # Viterbi algorithm
     hidden_states = model.predict(X)
     
-    return model, hidden_states
+    return model, hidden_states, log_returns
 
+def interpret_and_graph(model, hidden_states, prices, returns):
+    variances = np.array([model.covars_[i][0][0] for i in range(model.n_components)])
+    std_deviations = np.sqrt(variances)
+    sorted_indices = np.argsort(std_deviations)
+    
+    map_states = {old: new for new, old in enumerate(sorted_indices)}
+    new_states = []
+    
+    for hidden_state in hidden_states:
+        new_states.append(map_states[hidden_state])
+    new_states = np.array(new_states)
+    print(hidden_states)
+
+    colors = ['green', 'orange', 'red']
+    plt.figure(figsize=(15, 8))
+    
+    dates = returns.index
+    prices = prices.loc[dates]
+    
+    for state in range(model.n_components):
+        mask = (new_states == state)
+        plt.scatter(dates[mask], prices[mask], color=colors[state])
+        
+    plt.savefig("hmm_states.png")
+    
 if __name__ == "__main__":
     ticker = "NVDA"
     start_date = "2022-01-01"
@@ -30,4 +57,5 @@ if __name__ == "__main__":
     
     prices = fetch_stock_data(ticker, start_date, end_date)
     
-    model, hidden_states = fit_hmm_model(prices)
+    model, hidden_states, returns = fit_hmm_model(prices)
+    interpret_and_graph(model, hidden_states, prices, returns)
